c90861be8093171e6e9baee910cf86dceefba34a
Jonel Dominic Tapang <dneljo@yahoo.com>
2021-11-27 02:27:31 +0800

    FEAT: Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
-        <div className="expense-item__price">Amount</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
       </div>
     </div>
   );
