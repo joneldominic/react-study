0f198dd5ab9709c8656b20315496c5a1bf5d0d9e
Jonel Dominic Tapang <dneljo@yahoo.com>
2021-11-28 00:54:40 +0800

    FEAT: Bottom-up component data passing

--- src/App.js
+++ src/App.js
@@ -26,9 +26,13 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  };
+
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
